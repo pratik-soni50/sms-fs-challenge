edd3359a686900ad8a3d565c0b0d37b129e019cd
Pratik Soni <pratik.soni@gslab.com>
2020-04-27 12:37:18 +0530

    Setup env

--- api/src/utils/setUpDatabase.js
+++ api/src/utils/setUpDatabase.js
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose';
+import config from '../../config/env';
 
 export default async function setupDatabase() {
-  await mongoose.connect('mongodb://localhost:27017/account', {
+  await mongoose.connect(config.DB_URL || 'mongodb://localhost:27017/test', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: true,
